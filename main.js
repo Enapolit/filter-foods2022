30b59fba1bf3bcaf641f1ed057cdfb2dfb5802bf
Enapolit <90904490+Enapolit@users.noreply.github.com>
2022-03-22 18:25:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,20 @@ make_active.forEach(function (item) {
     item.classList.add("active");
 });
 });
+
+const select = document.querySelector("select.filter");
+
+select.addEventListener("change", function (e) {
+const filter = e.target.value;
+
+const actives = document.querySelectorAll(".item2.active");
+actives.forEach(function (item2) {
+    item.classList.remove("active");
+});
+
+const make_active = document.querySelectorAll(`.item2.${filter}`);
+
+make_active.forEach(function (item2) {
+    item.classList.add("active");
+});
+});
